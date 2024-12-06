9ffb9661d20205ad18f63e04c4169c5f16b7c4af
SvetlanaDmitrieva <sv-dmitrieva@list.ru>
2024-12-06 23:45:06 +0300

    06/12-01

--- PlayStore/task1/admin.py
+++ PlayStore/task1/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Buyer, Game, Post
+
+
+# Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    search_fields = ('name',)
+    list_filter = ('balance', 'age',)
+    fields = [('name', 'balance', 'age')]
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'age_limited')
+    search_fields = ('title',)
+    list_filter = ('cost', 'size', 'age_limited',)
+    fieldsets = (
+        ('Game Info', {
+            'fields': (('title', 'cost'),)
+        }),
+        ('Game Description', {
+            'fields': ('description', ('size', 'age_limited'),)
+        }),
+    )
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created_at', 'is_published')
+    search_fields = ('title',)
+    list_filter = ('is_published', 'created_at')
+    list_per_page = 10
+
--- PlayStore/task1/models.py
+++ PlayStore/task1/models.py
@@ -25,3 +25,14 @@ class Game(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+    objects = None
+    title = models.CharField(max_length=255, verbose_name= 'Заголовок')
+    content = models.TextField(verbose_name='Содержание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    def __str__(self):
+        return self.title
